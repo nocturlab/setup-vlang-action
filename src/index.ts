e5db0983aaa7061a0ec358c5ef43963f5f1e6450
Flavien CADET <flavien.cadet@nocturlab.fr>
2020-03-21 18:06:06 +0100

    Create v build workflow test (#2)
    * Create v build workflow test
    
    * Create action.yml
    
    * Create build_js.yml
    
    * WORKFLOW_COMMIT - update dist/index.js [skip ci]
    
    * add creation of vlang directory
    
    * add vlang dir in temp dir
    
    * add v executable to path
    
    * add V_HOME to path instead of v executable
    
    * WORKFLOW_COMMIT - update dist/index.js [skip ci]
    
    Co-authored-by: BOT_WORKFLOW <bot@nocturlab.fr>

---
+++ src/index.ts
@@ -0,0 +1,42 @@
+// Inspired by https://github.com/actions/setup-go
+
+import * as core from '@actions/core';
+import * as tc from '@actions/tool-cache';
+import * as installer from './installer';
+import * as path from 'path';
+import * as fs from 'fs';
+
+export async function run() {
+  try {
+    let v_version = core.getInput('v-version');
+
+    console.log(`Setup VLang with version ${v_version}`);
+
+    if (v_version) {
+      let cache_dir: string | undefined = tc.find('v', v_version);
+      let install_dir: string | undefined;
+
+      if (!cache_dir) {
+        console.log(`Vlang ${v_version} can't be found using cache, attempting to download ...`);
+        install_dir = await installer.download_v(v_version);
+        console.log(`VLang Installed to ${install_dir}`);
+      }
+
+      if (install_dir) {
+        core.exportVariable('V_HOME', install_dir);
+        core.addPath(install_dir);
+        console.log('Added VLang to the path');
+      } else {
+        throw new Error(`Could not find a version that satisfied version spec: ${v_version}`);
+      }
+    }
+
+    // add problem matchers
+    const matchersPath = path.join(__dirname, '..', 'matchers.json');
+    console.log(`##[add-matcher]${matchersPath}`);
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
+run();
---
+++ src/installer.ts
@@ -0,0 +1,43 @@
+import * as tc from '@actions/tool-cache';
+import * as path from 'path';
+import * as httpm from '@actions/http-client';
+import * as sys from './system';
+import {debug} from '@actions/core';
+
+export async function download_v(v_version: string): Promise<string | undefined> {
+  let download_path: string | undefined;
+  let ext_path: string | undefined;
+  let cache_path: string | undefined;
+
+  try {
+    // download
+    let download_url: string = `https://github.com/vlang/v/releases/`
+    if(v_version.includes('latest'))
+      download_url+= `${v_version}/download/v_${sys.getPlatform()}.zip`
+    else
+      download_url+= `download/${v_version}/v_${sys.getPlatform()}.zip`
+      
+    console.log(`Downloading VLang from ${download_url}`);
+
+    download_path = await tc.downloadTool(download_url);
+    console.log(`Vlang downloaded to ${download_path}`);
+  } catch (error) {
+    throw new Error(`Failed to download VLang version ${v_version}: ${error}`);
+  }
+  
+  try {
+    // extract
+    console.log('Extracting VLang...');
+    ext_path = await tc.extractZip(download_path, '/home/runner/work/_temp/vlang');
+    console.log(`VLang extracted to ${ext_path}`);
+
+    // extracts with a root folder that matches the fileName downloaded
+    console.log(`Add VLang to cache`);
+    cache_path = await tc.cacheDir(ext_path, 'v', v_version);
+    console.log(`VLang was added to cache using dir: ${cache_path}`);
+  } catch (error) {
+    throw new Error(`Failed to extract VLang version ${v_version}: ${error}`);
+  }
+
+  return cache_path;
+}
