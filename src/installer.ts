b4e2b7e27ae2bad8b0f5894abecd1fe0797dbb56
Shiishii <flavien.cadet@nocturlab.fr>
2020-03-21 16:10:30 +0000

    remove debug

--- src/installer.ts
+++ src/installer.ts
@@ -19,7 +19,7 @@ export async function download_v(v_version: string): Promise<string | undefined>
     console.log(`Downloading VLang from ${download_url}`);
 
     download_path = await tc.downloadTool(download_url);
-    debug(`Vlang downloaded to ${download_path}`);
+    console.log(`Vlang downloaded to ${download_path}`);
   } catch (error) {
     throw new Error(`Failed to download VLang version ${v_version}: ${error}`);
   }
@@ -27,15 +27,8 @@ export async function download_v(v_version: string): Promise<string | undefined>
   try {
     // extract
     console.log('Extracting VLang...');
-    fs.mkdirSync('/home/runner/work/_temp/vlang');
-    fs.readdir('/home/runner/work/_temp/', function(err, items) {
-      console.log(items);
-      for (var i=0; i<items.length; i++) {
-          console.log(items[i]);
-      }
-    });
     let ext_path: string = await tc.extractZip(download_path, '/home/runner/work/_temp/vlang');
-    debug(`VLang extracted to ${ext_path}`);
+    console.log(`VLang extracted to ${ext_path}`);
 
     // extracts with a root folder that matches the fileName downloaded
     const tool_root = path.join(ext_path, 'v');
