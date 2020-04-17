cda49c65ca0213583a6add81605d4844c9b40fc3
Flavien CADET <flavien.cadet@nocturlab.fr>
2020-04-17 23:12:48 +0200

    Just download the zip of Master branch if v_version is equal to master.

--- src/installer.ts
+++ src/installer.ts
@@ -14,7 +14,9 @@ export async function download_v(v_version: string): Promise<string | undefined>
     let download_url: string = `https://github.com/vlang/v/releases/`
     if(v_version.includes('latest'))
       download_url+= `${v_version}/download/v_${sys.getPlatform()}.zip`
-    else
+    else if(v_version.includes('master')){
+      download_url+= `https://github.com/vlang/v/archive/master.zip`
+    }else
       download_url+= `download/${v_version}/v_${sys.getPlatform()}.zip`
 
     console.log(`Downloading V from ${download_url}`);
