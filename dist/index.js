01b3caabb10f70fb89eb409e316c4fa722c5ce25
Flavien CADET <flavien.cadet@nocturlab.fr>
2020-12-10 14:53:04 +0100

    Revert "Remove the "v_" prefix from the zip (#71)"
    This reverts commit 5cb58a1a4ae31497272bea0547d2df257e208641.

--- dist/index.js
+++ dist/index.js
@@ -5050,12 +5050,12 @@ function download_v(v_version) {
             // download
             let download_url = `https://github.com/vlang/v/releases/`;
             if (v_version.includes('latest'))
-                download_url += `${v_version}/download/${sys.getPlatform()}.zip`;
+                download_url += `${v_version}/download/v_${sys.getPlatform()}.zip`;
             else if (v_version.includes('master')) {
                 download_url = `https://github.com/vlang/v/archive/master.zip`;
             }
             else
-                download_url += `download/${v_version}/${sys.getPlatform()}.zip`;
+                download_url += `download/${v_version}/v_${sys.getPlatform()}.zip`;
             console.log(`Downloading V from ${download_url}`);
             download_path = yield tc.downloadTool(download_url);
             console.log(`V downloaded to ${download_path}`);
